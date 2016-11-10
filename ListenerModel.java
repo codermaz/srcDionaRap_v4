ed0ed5107585586c87a0b1a60a7485d35226dba6
ms-161428 <ms-161428@ap055041.hs-weingarten.de>
2016-11-10 18:50:03 +0100

    ListenerModel v2

---
+++ ListenerModel.java
@@ -0,0 +1,23 @@
+
+
+import de.fhwgt.dionarap.model.events.DionaRapChangedEvent;
+import de.fhwgt.dionarap.model.events.GameStatusEvent;
+import de.fhwgt.dionarap.model.listener.DionaRapListener;
+
+public class ListenerModel implements DionaRapListener{
+
+	@Override
+	public void modelChanged(DionaRapChangedEvent arg0) {
+		
+		
+		
+	}
+
+	@Override
+	public void statusChanged(GameStatusEvent arg0) {
+		
+		
+		
+	}
+
+}
